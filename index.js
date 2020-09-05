0c43e417beb2579f28442d09b08b928b50a93e69
Davee Sok <daveesok@gmail.com>
2020-09-04 20:47:21 -0700

    added mongo connection for every route

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ require('dotenv').config();
 
 let server = require('./lib/server.js');
 
-
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 server.start(process.env.PORT);
